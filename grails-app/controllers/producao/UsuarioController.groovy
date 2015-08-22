9a57bef00ddc0654897095c11ff321398ec01a17
Carine Aguena <cca6@cin.ufpe.br>
2015-08-22 01:41:00 -0300

    encriptografando a senha dos logins

--- grails-app/controllers/producao/UsuarioController.groovy
+++ grails-app/controllers/producao/UsuarioController.groovy
@@ -1,13 +1,43 @@
 package producao
 
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
 class UsuarioController {
 
   def scaffold = Usuario
 
   def login = {}
+  
+    @Transactional
+    def save(Usuario userInstance) {
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userInstance.hasErrors()) {
+            respond userInstance.errors, view:'create'
+            return
+        }
+
+        userInstance.password = userInstance.password.encodeAsPassword()
+
+        userInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
+                redirect userInstance
+            }
+            '*' { respond userInstance, [status: CREATED] }
+        }
+    }
 
   def authenticate = {
-    def usuario = Usuario.findByLoginAndPassword(params.login, params.password)
+    def passwordHashed = params.password.encodeAsPassword()
+    def usuario = Usuario.findByLoginAndPassword(params.login, passwordHashed)
+    //def usuario = Usuario.findByLoginAndPassword(params.login, params.password)
     if(usuario){
       session.usuario = usuario
       //flash.message = "Olá ${usuario.name}!"
