054c638f87451db89d48afbe35c4b8371980c2b7
Carine Aguena <cca6@cin.ufpe.br>
2015-11-03 18:08:49 -0200

    atualizando o data source

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,8 +13,8 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
   //dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
-    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-//    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    //cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+   cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
